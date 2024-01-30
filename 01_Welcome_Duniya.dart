bec73b427887e9a3f66303a445f469f83f4de1d1
Pundalik Bhiva Desai <111678414+Dattaprasaddesai@users.noreply.github.com>
2024-01-30 23:00:33 +0530

    Create  01_Welcome_Duniya.dart

---
+++ 01_Welcome_Duniya.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	// This is my first line of code
+	print("Welcome Duniya!!!"); 	
+  
+	print("This is my first program");
+
+	// Performing arithematic operation
+	print(16 / 4);
+
+	// Printing out boolean value
+	print(false);
+}
